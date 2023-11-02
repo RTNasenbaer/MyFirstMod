e1a8f0f66201e7bf15cb8092feed47c73b899bae
rtnasenbaer <finn.martin@freenet.de>
2023-11-02 07:28:31 +0100

    Initial commit

---
+++ src/client/kotlin/me/rtnasenbaer/myfirstmod/MyFirstModClient.kt
@@ -0,0 +1,9 @@
+package me.rtnasenbaer.myfirstmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object MyFirstModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/me/rtnasenbaer/myfirstmod/MyFirstModDataGenerator.kt
@@ -0,0 +1,9 @@
+package me.rtnasenbaer.myfirstmod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MyFirstModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
