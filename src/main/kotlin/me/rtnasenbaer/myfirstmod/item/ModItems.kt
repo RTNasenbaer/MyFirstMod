49808ca82441f20fe60b646d4289eb15f122f370
rtnasenbaer <finn.martin@freenet.de>
2023-11-04 12:41:33 +0100

    new item

--- src/main/kotlin/me/rtnasenbaer/myfirstmod/item/ModItems.kt
+++ src/main/kotlin/me/rtnasenbaer/myfirstmod/item/ModItems.kt
@@ -27,9 +27,9 @@ object ModItems {
     val FOSSIL_AXE = registerItem("fossil_axe", AxeItem(ModToolMaterial.FOSSIL, 12F, 1F, FabricItemSettings().maxCount(1)))
     val FOSSIL_HOE = registerItem("fossil_hoe", HoeItem(ModToolMaterial.FOSSIL, 2, 4F, FabricItemSettings().maxCount(1)))
 
-    private fun addItemsToItemGroup(itemGroup: RegistryKey<ItemGroup>, item: Item) {
+    /*private fun addItemsToItemGroup(itemGroup: RegistryKey<ItemGroup>, item: Item) {
         ItemGroupEvents.modifyEntriesEvent(itemGroup).register { content -> content.add(item) }
-    }
+    }*/
 
     private fun registerItem(name : String) : Item {
         return Registry.register(Registries.ITEM, Identifier(MyFirstMod.MOD_ID, name), Item(FabricItemSettings()))
